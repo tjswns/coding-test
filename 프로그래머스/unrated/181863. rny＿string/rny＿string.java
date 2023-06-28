45a99a9eda7573335e1810ea7bf699ba31fd81eb
tjswns <111962386+tjswns@users.noreply.github.com>
2023-06-28 14:00:58 +0900

    [unrated] Title: rny_string, Time: 0.09 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String rny_string) {
+        return rny_string.replaceAll("m", "rn");
+        
+        // String answer = rny_string.replaceAll("m", "rn");
+        // return answer;
+    }
+}
