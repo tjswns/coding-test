c090176e969a65b15496019a20d018516419343b
tjswns <111962386+tjswns@users.noreply.github.com>
2023-07-21 09:28:00 +0900

    [unrated] Title: l로 만들기, Time: 6.40 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if (c < 'l') {
+                answer.append('l');
+            } else {
+                answer.append(c);
+            }
+        }
+
+        return answer.toString();
+    }
+}
